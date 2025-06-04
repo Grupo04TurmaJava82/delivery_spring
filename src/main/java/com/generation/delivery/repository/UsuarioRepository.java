97e56edc0924b565dfd5f3fe4880bc0e6e8c56d2
JuniorReis-dev <cleiton.nr47@gmail.com>
2025-06-04 11:26:39 -0300

    :sparkles: add anotaçao @repository e modificados arquivos na UsuarioService

--- src/main/java/com/generation/delivery/repository/UsuarioRepository.java
+++ src/main/java/com/generation/delivery/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.generation.delivery.repository;
 
 import com.generation.delivery.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByUsuario(String usuario);
